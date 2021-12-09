c3bcfa3073bdd825a20ec91a64bf6908dfef6e9c
Taeh83 <dreamsky83@naver.com>
2021-12-09 15:57:38 +0900

    Initial Commit

---
+++ testpager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testpager
+//
+//  Created by 홍태희 on 2021/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
